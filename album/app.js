9bc9e9ce5801c8ceb54eb6bf243753535aef73b0
xuxiaozhi <309123793@qq.com>
2017-12-13 16:25:43 +0800

    上传图片

--- album/app.js
+++ album/app.js
@@ -11,6 +11,10 @@ app.use(express.static("./uploads"));
 app.get("/",router.showIndex);
 app.get("/:albumName",router.showAlbum);
 
+app.get("/up",router.showUp);
+
+app.post("/up",router.doPost);
+
 app.use(function (req,res) {
     res.render('404');
 })
--- album/controller/router.js
+++ album/controller/router.js
@@ -1,4 +1,7 @@
 var file=require("../models/file.js");
+var formidable = require('formidable');
+var path=require("path");
+var fs=require("fs");
 
 exports.showIndex=function (req,res) {
     file.getAlbums(function (err,allAlbums) {
@@ -24,4 +27,53 @@ exports.showAlbum=function (req,res,next) {
             "images":imagesArray
         })
     })
+}
+
+exports.showUp=function (req,res) {
+    file.getAlbums(function (err,allAlbums) {
+        if(err){
+            res.send(err);
+            return;
+        }
+        res.render('up',{
+            "album":allAlbums
+        })
+    })
+
+}
+
+exports.doPost=function (req,res,next) {
+    var form = new formidable.IncomingForm();
+    form.uploadDir = path.normalize(__dirname+"/../temp");
+    form.parse(req, function(err, fields, files) {
+        console.log(fields);
+        console.log(files);
+
+        //改名
+        if(err){
+            next();
+            return;
+        }
+
+        var size=parseInt(files.image.size);
+        if(size>1024){
+            fs.unlink(files.image.path);
+            return;
+        }
+
+        var ttt=parseInt(Math.random()*89999+10000);
+        var ext=path.extname(files.image.name)
+        var folder=fields.folder;
+        var oldPath=files.image.path;
+        var newPath=path.normalize(__dirname+"/../uploads/"+folder+"/"+ttt+ext);
+        fs.rename(oldPath,newPath,function (err) {
+            if(err){
+                res.send("改名失败");
+            }
+        })
+
+
+    });
+
+    res.send("成功");
 }
